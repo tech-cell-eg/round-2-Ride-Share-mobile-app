3152877fec92800f136d5d0525fee23f103e0c7d
Shiba <just.shiba3006@gmail.com>
2025-02-03 10:00:05 +0200

    Refactor LocationConfirmScreenBody to use StatelessWidget and enhance UI with notification icon

--- lib/features/contact and payment/presentation/views/widgets/location_confirm_screen_body.dart
+++ lib/features/contact and payment/presentation/views/widgets/location_confirm_screen_body.dart
@@ -5,17 +5,45 @@ import 'package:yandex_maps_mapkit_lite/yandex_map.dart';
 
 import 'custom_map.dart';
 
-class LocationConfirmScreenBody extends StatefulWidget {
+class LocationConfirmScreenBody extends StatelessWidget {
   const LocationConfirmScreenBody({super.key});
 
   @override
-  State<LocationConfirmScreenBody> createState() =>
-      _LocationConfirmScreenBodyState();
-}
-
-class _LocationConfirmScreenBodyState extends State<LocationConfirmScreenBody> {
-  @override
   Widget build(BuildContext context) {
-    return CustomMap();
+    return Stack(
+      children: [
+        CustomMap(),
+        Positioned(
+          top: 60,
+          right: 15,
+          left: 15,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Icon(
+                Icons.draw_outlined,
+                size: 30,
+              ),
+              InkWell(
+                //Background Color/BgLight
+                onTap: () {},
+                child: Container(
+                  height: 34,
+                  width: 34,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Icon(
+                    Icons.notifications_outlined,
+                    size: 30,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
