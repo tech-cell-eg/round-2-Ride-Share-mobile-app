bb9dead7f4232dfc99efc54a9d714c7c79604461
Shiba <just.shiba3006@gmail.com>
2025-02-09 21:49:49 +0200

    Add InfoLineRow widget and integrate it into PaymentScreenBody

--- lib/payment/presentation/views/widgets/payment_screen_body.dart
+++ lib/payment/presentation/views/widgets/payment_screen_body.dart
@@ -4,6 +4,7 @@ import 'package:ride_share/core/constants/generated/images.dart';
 import 'package:ride_share/core/widgets/green_box.dart';
 
 import 'driver_rate_box.dart';
+import 'info_line_row.dart';
 
 class PaymentScreenBody extends StatelessWidget {
   const PaymentScreenBody({super.key});
@@ -13,6 +14,7 @@ class PaymentScreenBody extends StatelessWidget {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           SizedBox(height: 30),
           DriverRateBox(),
@@ -25,6 +27,22 @@ class PaymentScreenBody extends StatelessWidget {
             ),
           ),
           SizedBox(height: 10),
+          InfoLineRow(
+            title: 'Mustang/',
+            subTitle: 'per hours',
+            amountTitle: r'$200',
+          ),
+          SizedBox(height: 10),
+          InfoLineRow(
+            title: 'Vat',
+            subTitle: '  (5%)',
+            amountTitle: r'$20',
+          ),
+          InfoLineRow(
+            title: 'Total',
+            subTitle: '',
+            amountTitle: r'$220',
+          ),
         ],
       ),
     );
