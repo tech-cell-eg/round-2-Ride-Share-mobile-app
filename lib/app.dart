d8ac3da9b449f5b072ffb271a9fe196e726f6cf8
Amr-3li <amrali51111@gmail.com>
2025-02-09 18:26:10 +0200

    some edits

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:ride_share/Features/list_transportion/list_transportion_page.dart';
-=======
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ride_share/core/constants/app_colors.dart';
 
@@ -10,7 +8,6 @@ import 'features/Auth/send_verification/send_verification_screen.dart';
 import 'features/Auth/set_new_password/set_new_password_screen.dart';
 import 'features/Auth/sign_in/signin_screen.dart';
 import 'features/welcome_screen/welcome_screen.dart';
->>>>>>> ecbee85f9e55ebc417a356ead2da47a11eb70472
 
 import 'core/config/routing/app_router.dart';
 import 'features/contact and payment/presentation/views/location_confirm_screen.dart';
@@ -20,42 +17,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        fontFamily: 'Poppins',
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTransportionPage();
-=======
     return ScreenUtilInit(
       designSize: const Size(360, 690),
       minTextAdapt: true,
@@ -70,6 +31,5 @@ class MyHomePage extends StatelessWidget {
         initialRoute: LocationConfirmScreen.routeName,
       ),
     );
->>>>>>> ecbee85f9e55ebc417a356ead2da47a11eb70472
   }
 }
