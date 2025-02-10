621b9dc7185bbd963414a66d8d5c4a3bce99ab36
Mohamed Hassan <mohamed7github@gmail.com>
2025-02-10 20:39:09 +0200

    finish Complain Screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:ride_share/core/theme/app_theme.dart';
-import 'package:ride_share/features/complain/presentation/screens/complain_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -13,7 +12,6 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.system,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      home: ComplainScreen(),
     );
   }
 }
