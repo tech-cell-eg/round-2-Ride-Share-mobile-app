e73749021f5650cb256d5a4ea5dc215e070a4d43
Mohamed Hassan <mohamed7github@gmail.com>
2025-02-10 17:51:10 +0200

    refactore wallet screens - Add AppStrings file

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,40 @@
+abstract class AppStrings {
+  AppStrings._();
+
+  static const String successDialogTitle = 'Add Success';
+
+  static const String successDialogSubTitle =
+      'Your money has been add successfully ';
+
+  static const String amount = 'Amount';
+
+  static const String backHomeButtonTitle = 'Back Home';
+
+  static const String enterAmount = 'Enter Amount';
+
+  static const String addPaymentMethod = 'Add payment Method';
+
+  static const String selectPaymentMethod = 'Select Payment Method';
+
+  static const String confirm = 'Confirm';
+
+  static const String wallet = 'Wallet';
+
+  static const String transactions = 'Transactions';
+
+  static const String  availableBalance = 'Available Balance';
+
+  static const String  totalExpend = 'Total Expend';
+
+  static const String addMoney = 'Add Money';
+
+  static const String seeAll = 'See All';
+
+  static const String welton = 'Welton';
+  static const String nathsam = 'Nathsam';
+
+  static const String accountNumber = 'Account Number';
+
+  static const String savePaymentMethod = 'Save Payment Method';
+
+}
--- lib/features/wallet/presentation/widgets/wallet_money/wallet_money_section.dart
+++ lib/features/wallet/presentation/widgets/wallet_money/wallet_money_section.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ride_share/core/constants/app_sizes.dart';
+import 'package:ride_share/core/constants/app_strings.dart';
 import 'package:ride_share/features/wallet/presentation/widgets/wallet_money/wallet_money_card.dart';
 
 class WalletMoneySection extends StatelessWidget {
@@ -12,14 +13,14 @@ class WalletMoneySection extends StatelessWidget {
         Expanded(
           child: WalletMonyCard(
             moneyQuantity: 500,
-            title: 'Available Balance',
+            title: AppStrings.availableBalance,
           ),
         ),
         const SizedBox(width: AppSizes.spaceBtwWalletCards),
         Expanded(
           child: WalletMonyCard(
             moneyQuantity: 200,
-            title: 'Total Expend',
+            title: AppStrings.totalExpend,
           ),
         ),
       ],
