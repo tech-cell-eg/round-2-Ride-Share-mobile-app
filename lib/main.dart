a4af9d9a8e561dd4cc676f3f5d52e09187f01522
Mohamed Hassan <mohamed7github@gmail.com>
2025-02-10 16:51:11 +0200

    Create home Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:ride_share/app.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: false,
+      builder: (context) => MyApp(),
+    ),
+  );
 }
-
