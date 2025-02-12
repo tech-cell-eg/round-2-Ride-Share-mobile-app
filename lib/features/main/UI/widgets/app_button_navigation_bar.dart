afd71d31ca7d07480c4686f6b8022b2ac4f23cc5
Mohamed Hassan <mohamed7github@gmail.com>
2025-02-13 01:11:14 +0200

    Enhance AppBottomNavigationBar: adjust background color based on theme and clean up commented code

--- lib/features/main/UI/widgets/app_button_navigation_bar.dart
+++ lib/features/main/UI/widgets/app_button_navigation_bar.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ride_share/core/constants/app_strings.dart';
 import 'package:ride_share/core/constants/app_text_styles.dart';
-
 import 'package:ride_share/features/main/UI/widgets/nav_bar_item.dart';
-
 import '../../../../core/constants/generated/icons.dart';
 
 class AppBottomNavigationBar extends StatelessWidget {
@@ -16,6 +14,7 @@ class AppBottomNavigationBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
     return ClipRRect(
       borderRadius: const BorderRadius.only(
         topLeft: Radius.circular(40),
@@ -23,6 +22,7 @@ class AppBottomNavigationBar extends StatelessWidget {
       ),
       child: BottomAppBar(
         clipBehavior: Clip.hardEdge,
+        color: isDark ? const Color.fromARGB(255, 20, 20, 20) : Colors.white,
         child: ValueListenableBuilder<int>(
           valueListenable: selectedIndex,
           builder: (context, index, child) {
@@ -58,6 +58,12 @@ class AppBottomNavigationBar extends StatelessWidget {
                       ),
                     ],
                   ),
+                  // NavBarItem(
+                  //   iconPath: MyIcons.wallet,
+                  //   pageName: AppStrings.offers,
+                  //   isSelected: index == 2,
+                  //   onTap: () => selectedIndex.value = 2,
+                  // ),
                   NavBarItem(
                     iconPath: MyIcons.offer,
                     pageName: AppStrings.offers,
