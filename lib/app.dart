34b56942bec64f1fb04efc2d882c96e085dc67e9
Mohamed Hassan <mohamed7github@gmail.com>
2025-02-15 16:37:13 +0200

    Refactor keyboard dismissal observer: move to new directory and enhance implementation

--- lib/app.dart
+++ lib/app.dart
@@ -3,7 +3,7 @@ import 'package:ride_share/core/config/routing/app_router.dart';
 import 'package:ride_share/core/config/routing/routes.dart';
 import 'package:ride_share/core/theme/app_theme.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:ride_share/core/utils/helpers/keyboard/keyboard_dismiss_observer.dart';
+import 'package:ride_share/core/utils/helpers/observers/keyboard/keyboard_dismiss_observer.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
